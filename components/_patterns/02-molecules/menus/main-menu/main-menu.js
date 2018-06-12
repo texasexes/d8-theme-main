ff935eec9751d09835831db38506764c5034c866
Brian Lewis <brian@modulesunraveled.com>
2018-06-11 23:11:13 -0500

    WIP: mobile menu interaction.

--- components/_patterns/02-molecules/menus/main-menu/main-menu.js
+++ components/_patterns/02-molecules/menus/main-menu/main-menu.js
@@ -1,39 +1,67 @@
 (function ($, Drupal) {
   // Element selectors
-  const mainMenuToggle = '.main-menu-toggle';
-  const mainMenuToggleOpen = '.main-menu-toggle--open';
-  const mainMenuToggleIcon = '.main-menu-toggle__icon';
-  const mainMenu = '.main-nav';
-  const mainMenuSub = '.main-menu__expand-sub';
+  const mainMenuToggle = $('.main-menu-toggle');
+  const mainMenuToggleOpen = $('.main-menu-toggle--open');
+  const mainMenu = $('.main-nav');
+  const mainMenuSub = $('.main-menu__expand-sub');
+  const toolbar = $('#toolbar-administration');
+  const body = $('body');
+
   // Classes
   const mainMenuSubOpen = 'main-menu__expand-sub--open';
   const mainMenuVisible = 'js-visible';
   const mainMenuActive = 'js-menu-active';
+  const noscroll = 'noscroll';
+
   // New elements
   let mainMenuBg = '';
 
+  // Determine how far down we're scrolled
+  let scrolled = '0';
+  console.log(scrolled);
+  let adjust = '10';
+
+  // Account for admin menu
+  let toolbarHeight = '0';
+
+  if (toolbar.length) {
+    toolbarHeight = toolbar.outerHeight();
+    mainMenu.css({top: toolbarHeight});
+  }
+
+  // Show/Hide overlay
   function mainMenuBgToggle() {
     if (mainMenuBg.length) {
-      $(mainMenuBg).remove();
+      mainMenuBg.remove();
       mainMenuBg = '';
+      scrolled = $(document).scrollTop();
+      adjust = scrolled * -1;
+      body.css({top: adjust});
     }
 
     else {
       mainMenuBg = $('<span class="js-main-menu-close" />');
-      $(mainMenuToggleOpen).append(mainMenuBg);
+      mainMenuBg.css({top: toolbarHeight});
+      mainMenuToggleOpen.append(mainMenuBg);
     }
   };
 
   // Main Menu
-  $(mainMenuToggle).click(function (e) {
+  mainMenuToggle.click(function (e) {
     e.preventDefault();
+    scrolled = $(document).scrollTop();
+    adjust = scrolled * -1;
+    console.log(scrolled);
+    body.css({top: adjust});
+    body.toggleClass(noscroll);
     // Display Main Nav
-    $(mainMenu).toggleClass(mainMenuVisible);
+    mainMenu.toggleClass(mainMenuVisible);
+    // Toggle Overlay
     mainMenuBgToggle();
   });
 
   // Display Sub-nav
-  $(mainMenuSub).click(function (e) {
+  mainMenuSub.click(function (e) {
     e.preventDefault();
     $(this).toggleClass(mainMenuSubOpen);
     $(this).next('ul').toggleClass(mainMenuVisible);
--- dist/scripts-styleguide.js
+++ dist/scripts-styleguide.js
@@ -2,38 +2,66 @@
 
 (function ($, Drupal) {
   // Element selectors
-  var mainMenuToggle = '.main-menu-toggle';
-  var mainMenuToggleOpen = '.main-menu-toggle--open';
-  var mainMenuToggleIcon = '.main-menu-toggle__icon';
-  var mainMenu = '.main-nav';
-  var mainMenuSub = '.main-menu__expand-sub';
+  var mainMenuToggle = $('.main-menu-toggle');
+  var mainMenuToggleOpen = $('.main-menu-toggle--open');
+  var mainMenu = $('.main-nav');
+  var mainMenuSub = $('.main-menu__expand-sub');
+  var toolbar = $('#toolbar-administration');
+  var body = $('body');
+
   // Classes
   var mainMenuSubOpen = 'main-menu__expand-sub--open';
   var mainMenuVisible = 'js-visible';
   var mainMenuActive = 'js-menu-active';
+  var noscroll = 'noscroll';
+
   // New elements
   var mainMenuBg = '';
 
+  // Determine how far down we're scrolled
+  var scrolled = '0';
+  console.log(scrolled);
+  var adjust = '10';
+
+  // Account for admin menu
+  var toolbarHeight = '0';
+
+  if (toolbar.length) {
+    toolbarHeight = toolbar.outerHeight();
+    mainMenu.css({ top: toolbarHeight });
+  }
+
+  // Show/Hide overlay
   function mainMenuBgToggle() {
     if (mainMenuBg.length) {
-      $(mainMenuBg).remove();
+      mainMenuBg.remove();
       mainMenuBg = '';
+      scrolled = $(document).scrollTop();
+      adjust = scrolled * -1;
+      body.css({ top: adjust });
     } else {
       mainMenuBg = $('<span class="js-main-menu-close" />');
-      $(mainMenuToggleOpen).append(mainMenuBg);
+      mainMenuBg.css({ top: toolbarHeight });
+      mainMenuToggleOpen.append(mainMenuBg);
     }
   };
 
   // Main Menu
-  $(mainMenuToggle).click(function (e) {
+  mainMenuToggle.click(function (e) {
     e.preventDefault();
+    scrolled = $(document).scrollTop();
+    adjust = scrolled * -1;
+    console.log(scrolled);
+    body.css({ top: adjust });
+    body.toggleClass(noscroll);
     // Display Main Nav
-    $(mainMenu).toggleClass(mainMenuVisible);
+    mainMenu.toggleClass(mainMenuVisible);
+    // Toggle Overlay
     mainMenuBgToggle();
   });
 
   // Display Sub-nav
-  $(mainMenuSub).click(function (e) {
+  mainMenuSub.click(function (e) {
     e.preventDefault();
     $(this).toggleClass(mainMenuSubOpen);
     $(this).next('ul').toggleClass(mainMenuVisible);
