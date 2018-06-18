a7113d56144e94fc949b6bfc5d383ebd8ff71535
Brian Lewis <brian@modulesunraveled.com>
2018-06-18 00:05:14 -0500

    Finalized initial accordion component.

--- components/_patterns/02-molecules/accordion/accordion.js
+++ components/_patterns/02-molecules/accordion/accordion.js
@@ -1,58 +1,16 @@
-// // UNCOMMENT IF DRUPAL - see components/_meta/_01-foot.twig for attachBehaviors
-// // Drupal.behaviors.accordion = {
-// //   attach: function (context, settings) {
+(function ($, Drupal) {
 
-// (function () { // REMOVE IF DRUPAL
+  const accordionHeading = $('.accordion__heading');
+  const accordionContent = $('.accordion__content');
+  const activeClass = 'js-active';
+  const hiddenClass = 'js-hidden';
 
-//   'use strict';
+  accordionContent.addClass(hiddenClass);
 
-//   // Set 'document' to 'context' if Drupal
-//   var accordionTerm = document.querySelectorAll('.accordion-term');
-//   var accordionDef = document.querySelectorAll('.accordion-def');
+  accordionHeading.click(function (e) {
+    e.preventDefault();
+    $(this).toggleClass(activeClass);
+    $(this).next(accordionContent).toggleClass(hiddenClass);
+  });
 
-//   // If javascript, hide accordion definition on load
-//   function jsCheck() {
-//     for (var i = 0; i < accordionDef.length; i++) {
-//       if (accordionDef[i].classList) {
-//         accordionDef[i].classList.add('active');
-//         accordionDef[0].previousElementSibling.classList.add('is-active');
-//       }
-//       else {
-//         accordionDef[i].className += ' active';
-//         accordionDef[0].previousElementSibling.classList.add('is-active');
-//       }
-//     }
-//   }
-
-//   jsCheck();
-
-//   // Accordion Toggle
-//   // Mobile Click Menu Transition
-//   for (var i = 0; i < accordionTerm.length; i++) {
-//     accordionTerm[i].addEventListener('click', function (e) {
-//       var className = 'is-active';
-//       // Add is-active class
-//       if (this.classList) {
-//         this.classList.toggle(className);
-//       }
-//       else {
-//         var classes = this.className.split(' ');
-//         var existingIndex = classes.indexOf(className);
-
-//         if (existingIndex >= 0) {
-//           classes.splice(existingIndex, 1);
-//         }
-//         else {
-//           classes.push(className);
-//         }
-//         this.className = classes.join(' ');
-//       }
-//       e.preventDefault();
-//     });
-//   }
-
-// })(); // REMOVE IF DRUPAL
-
-// // UNCOMMENT IF DRUPAL
-// //   }
-// // };
+}(jQuery, Drupal));
--- dist/scripts-styleguide.js
+++ dist/scripts-styleguide.js
@@ -1,60 +1,18 @@
-// // UNCOMMENT IF DRUPAL - see components/_meta/_01-foot.twig for attachBehaviors
-// // Drupal.behaviors.accordion = {
-// //   attach: function (context, settings) {
+'use strict';
 
-// (function () { // REMOVE IF DRUPAL
+(function ($, Drupal) {
 
-//   'use strict';
+  var accordionHeading = $('.accordion__heading');
+  var accordionContent = $('.accordion__content');
+  var activeClass = 'js-active';
+  var hiddenClass = 'js-hidden';
 
-//   // Set 'document' to 'context' if Drupal
-//   var accordionTerm = document.querySelectorAll('.accordion-term');
-//   var accordionDef = document.querySelectorAll('.accordion-def');
+  accordionContent.addClass(hiddenClass);
 
-//   // If javascript, hide accordion definition on load
-//   function jsCheck() {
-//     for (var i = 0; i < accordionDef.length; i++) {
-//       if (accordionDef[i].classList) {
-//         accordionDef[i].classList.add('active');
-//         accordionDef[0].previousElementSibling.classList.add('is-active');
-//       }
-//       else {
-//         accordionDef[i].className += ' active';
-//         accordionDef[0].previousElementSibling.classList.add('is-active');
-//       }
-//     }
-//   }
-
-//   jsCheck();
-
-//   // Accordion Toggle
-//   // Mobile Click Menu Transition
-//   for (var i = 0; i < accordionTerm.length; i++) {
-//     accordionTerm[i].addEventListener('click', function (e) {
-//       var className = 'is-active';
-//       // Add is-active class
-//       if (this.classList) {
-//         this.classList.toggle(className);
-//       }
-//       else {
-//         var classes = this.className.split(' ');
-//         var existingIndex = classes.indexOf(className);
-
-//         if (existingIndex >= 0) {
-//           classes.splice(existingIndex, 1);
-//         }
-//         else {
-//           classes.push(className);
-//         }
-//         this.className = classes.join(' ');
-//       }
-//       e.preventDefault();
-//     });
-//   }
-
-// })(); // REMOVE IF DRUPAL
-
-// // UNCOMMENT IF DRUPAL
-// //   }
-// // };
-"use strict";
+  accordionHeading.click(function (e) {
+    e.preventDefault();
+    $(this).toggleClass(activeClass);
+    $(this).next(accordionContent).toggleClass(hiddenClass);
+  });
+})(jQuery, Drupal);
 //# sourceMappingURL=scripts-styleguide.js.map
