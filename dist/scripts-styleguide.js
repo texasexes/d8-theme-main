9857a8819033a693bcf94bdb88fd05addd752b05
Jeff Cardwell <jeff-cardwell@users.noreply.github.com>
2019-07-24 18:11:12 -0500

    fresh dist/ for a fresh build

--- dist/scripts-styleguide.js
+++ dist/scripts-styleguide.js
@@ -2,19 +2,106 @@
 
 (function ($, Drupal) {
 
+  // WaitForFinalEvent Function
+  // This function allow us to only perform re-calculations AFTER an event has copmleted.
+  // Below, we'll use it to recalculate values only after the window has been resized, instead of recalculating every pixel change.
+  // Usage:
+  // $(window).resize(function () {
+  //   waitForFinalEvent(function(){
+  //     alert('Resize...');
+  //     //...
+  //   }, 500, "some unique string");
+  // });
+  var waitForFinalEvent = function () {
+    var timers = {};
+    return function (callback, ms, uniqueId) {
+      if (!uniqueId) {
+        uniqueId = "Don't call this twice without a uniqueId";
+      }
+      if (timers[uniqueId]) {
+        clearTimeout(timers[uniqueId]);
+      }
+      timers[uniqueId] = setTimeout(callback, ms);
+    };
+  }();
+
+  var accordionTabsItem = $('.tabs .accordion__item');
+  var accordionTabsItemContent = $('.tabs .accordion__item .accordion__content');
+  var accordionTabsItemHeading = $('.tabs .accordion__item .accordion__heading');
+  var accordionItem = $('.accordion__item');
   var accordionHeading = $('.accordion__heading');
   var accordionContent = $('.accordion__content');
   var accordionIcon = $('.accordion__expand-icon');
   var activeClass = 'js-active';
   var hiddenClass = 'js-hidden';
+  var openClass = 'js-open';
+  var closedClass = 'js-closed';
+  var win = $(window);
+
+  var winW = parseInt(win.width(), 10);
+
+  var maxHeight = function maxHeight(elems) {
+    return Math.max.apply(null, elems.map(function () {
+      return $(this).outerHeight();
+    }).get());
+  };
+
+  var maxTabsHeadingHeight = maxHeight(accordionTabsItemHeading);
+  var maxTabsContentHeight = maxHeight(accordionTabsItemContent);
+  var maxTabHeight = maxTabsHeadingHeight + maxTabsContentHeight;
+
+  accordionTabsItem.outerHeight(maxTabHeight);
+  // Add extra pixel to make header overlap content and override it's border.
+  accordionTabsItemHeading.outerHeight(parseInt(maxTabsHeadingHeight) + 1);
+  accordionTabsItemContent.css({ top: maxTabsHeadingHeight });
+
+  var maxHeadingHeight = maxHeight(accordionHeading);
+  var maxIconHeight = maxHeight(accordionIcon);
+
+  var accordionTopOffset = parseInt(maxHeadingHeight) / 2 - parseInt(maxIconHeight) / 2;
+  accordionIcon.css({ top: accordionTopOffset });
 
   // Hide content via js so that if js is disabled, all of the content is visible
-  accordionContent.addClass(hiddenClass);
+  accordionItem.addClass(closedClass);
+  accordionItem.children().addClass(closedClass);
+
+  // Initialize tabs if needed
+  accordionTabsItem.first().addClass(openClass).removeClass(closedClass);
+  accordionTabsItem.first().children().addClass(openClass).removeClass(closedClass);
 
   accordionHeading.click(function (e) {
     e.preventDefault();
-    $(this).siblings().toggleClass(activeClass);
-    $(this).siblings().toggleClass(hiddenClass);
+    $(this).parent().addClass(openClass).removeClass(closedClass);
+    $(this).parent().children().addClass(openClass).removeClass(closedClass);
+
+    $(this).parent().siblings().addClass(closedClass).removeClass(openClass);
+    $(this).parent().siblings().children().addClass(closedClass).removeClass(openClass);
+  });
+
+  // Update everything when the window is resized
+  win.resize(function () {
+    waitForFinalEvent(function () {
+      winW = parseInt(win.width(), 10);
+      // If desktop width set height
+      // Clear manually set css and height
+      accordionTabsItem.height('');
+      accordionTabsItemContent.css({ top: '' });
+      accordionTabsItemHeading.height('');
+
+      // Always position icon
+      accordionIcon.css({ top: parseInt(maxTabsHeadingHeight) / 2 });
+
+      if (winW > 900) {
+        maxTabsHeadingHeight = maxHeight(accordionTabsItemHeading);
+        maxTabsContentHeight = maxHeight(accordionTabsItemContent);
+        maxTabHeight = maxTabsHeadingHeight + maxTabsContentHeight;
+
+        accordionTabsItem.outerHeight(maxTabHeight);
+        // Add extra pixel to make header overlap content and override it's border.
+        accordionTabsItemHeading.outerHeight(parseInt(maxTabsHeadingHeight) + 1);
+        accordionTabsItemContent.css({ top: maxTabsHeadingHeight });
+      }
+    }, 100, "Accordion - window resize");
   });
 })(jQuery, Drupal);
 'use strict';
