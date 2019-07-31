36b96e71f70e96bd6429add2c716db1809df35f0
Jeff Cardwell <jeff-cardwell@users.noreply.github.com>
2019-07-31 14:25:25 -0500

    fresh dist/ for fresh build

--- dist/scripts-styleguide.js
+++ dist/scripts-styleguide.js
@@ -25,6 +25,8 @@
     };
   }();
 
+  var dom = $(document);
+  var win = $(window);
   var accordionTabsItem = $('.tabs .accordion__item');
   var accordionTabsItemContent = $('.tabs .accordion__item .accordion__content');
   var accordionTabsItemHeading = $('.tabs .accordion__item .accordion__heading');
@@ -36,8 +38,18 @@
   var hiddenClass = 'js-hidden';
   var openClass = 'js-open';
   var closedClass = 'js-closed';
-  var win = $(window);
   var breakpoint = 900;
+
+  var toolbarAdministration = null;
+  var toolbarTrayHorizontal = null;
+  var toolbarOffset = null;
+  var headerWrapperScrollUp = null;
+  var headerWrapper = null;
+  var headerWrapperOffset = null;
+  var additionalOffset = 0;
+  var bodyFixedAdminBar = null;
+  var bodyFixedAdminHorizontalTray = null;
+  var bodyFixedAdminBarHeight = 0;
 
   var winW = parseInt(win.outerWidth(), 10);
 
@@ -46,6 +58,22 @@
       return $(this).outerHeight();
     }).get());
   };
+
+  // Calculate body padding top (to accommodate the admin menu)
+  function calculateBodyFixedAdminBarHeight() {
+    bodyFixedAdminBar = $('body.toolbar-fixed #toolbar-bar');
+    bodyFixedAdminHorizontalTray = $('body.toolbar-fixed.toolbar-tray-open.toolbar-horizontal #toolbar-item-administration-tray');
+    bodyFixedAdminBarHeight = (parseInt(bodyFixedAdminBar.outerHeight()) || 0) + (parseInt(bodyFixedAdminHorizontalTray.outerHeight()) || 0);
+  };
+
+  function calculateHeights() {
+    dom.ready(function () {
+      // Handle fixed Drupal admin bar.
+      calculateBodyFixedAdminBarHeight();
+    });
+  };
+
+  calculateHeights();
 
   var maxTabsHeadingHeight = maxHeight(accordionTabsItemHeading);
   var maxTabsContentHeight = maxHeight(accordionTabsItemContent);
@@ -76,30 +104,56 @@
   accordionHeading.click(function (e) {
     e.preventDefault();
 
-    if (accordionTabsItem.length && winW > breakpoint) {
-      // Tabs are active. Click does not toggle, just sets to open.
-      $(this).parent().addClass(openClass).removeClass(closedClass);
-      $(this).parent().children().addClass(openClass).removeClass(closedClass);
-    } else {
-      // Toggle click element open/closed.
-      $(this).parent().toggleClass(openClass).toggleClass(closedClass);
-      $(this).parent().children().toggleClass(openClass).toggleClass(closedClass);
-    }
-
-    // Close all other elements.
+    // Close all other elements but self.
     $(this).parent().siblings().addClass(closedClass).removeClass(openClass);
     $(this).parent().siblings().children().addClass(closedClass).removeClass(openClass);
+
+    // When accordion uses tabs display and window is wider than breakpoint.
+    // Keep in mind that tabs styles are only applied when wider than breakpoint
+    // in scss files.
+    if (accordionTabsItem.length && winW > breakpoint) {
+      // Click does not toggle, just sets to open.
+      $(this).parent().addClass(openClass).removeClass(closedClass);
+      $(this).parent().children().addClass(openClass).removeClass(closedClass);
+      // This is default accordion behavior (no tabs display).
+    } else {
+      // Click toggles element open/closed.
+      $(this).parent().toggleClass(openClass).toggleClass(closedClass);
+      $(this).parent().children().toggleClass(openClass).toggleClass(closedClass);
+
+      if (winW <= breakpoint) {
+        // Handle scroll behavior offset in relation to scroll-hide mobile menu bar.
+        headerWrapper = $('.header-wrapper');
+        headerWrapperScrollUp = $('.header-wrapper .scrollUp');
+        headerWrapperOffset = (parseInt(headerWrapper.outerHeight()) || 0) - (parseInt(headerWrapperScrollUp.outerHeight()) || 0);
+
+        if ($(this).offset().top < win.scrollTop() + headerWrapperOffset) {
+          additionalOffset = headerWrapperOffset;
+        }
+      }
+
+      calculateBodyFixedAdminBarHeight();
+      additionalOffset += bodyFixedAdminBarHeight;
+
+      // Scroll behavior when an accordion item is clicked.
+      $('html,body').animate({
+        scrollTop: $(this).offset().top - additionalOffset
+      }, 500);
+      // Re-Initialize scroll additional offset for next click.
+      additionalOffset = 0;
+    }
   });
 
   // Update everything when the window is resized
   win.resize(function () {
     waitForFinalEvent(function () {
       winW = parseInt(win.outerWidth(), 10);
-      // If desktop width set height
       // Clear manually set css and height
       accordionTabsItem.outerHeight('');
       accordionTabsItemHeading.outerHeight('');
       accordionTabsItemContent.css({ top: '' });
+
+      calculateHeights();
 
       // Always position icon
       accordionIcon.css({ top: parseInt(maxTabsHeadingHeight) / 2 });
@@ -163,19 +217,26 @@
   var header = $('.header-wrapper');
   var contentTopRegion = $('.content-top-region');
   var layoutContainer = $('.layout-container');
+  var adminToolbar = $('#toolbar-bar');
 
   // Calculated values
-  var winH = parseInt(win.height(), 10);
-  var winW = parseInt(win.width(), 10);
-  var bodyPaddingTop = 0;
+  var winH = parseInt(win.outerHeight(), 10);
+  var winW = parseInt(win.outerWidth(), 10);
   var headerHeight = 0;
   var maxHeight = '100vh';
   var mainMenuHeight = mainMenu.outerHeight();
-  var mainMenuActiveItem = $('.is-active');
+  // Needs .main-menu__item--active-trail-last in case menu item is in menu
+  // twice. Always scrolls to "main" menu item (the one designated on the entity).
+  var mainMenuActiveItem = $('#main-nav .main-menu__item--active-trail-last .is-active');
   var dummyElement = $('');
   var dummyElementMaxHeight = '0';
   var currentScrollTop = 0;
   var totalHeightToSkip = 0;
+  var adminToolbarHeight = parseInt(adminToolbar.outerHeight()) || 0;
+  var bodyFixedAdminBarHeight = 0;
+  var bodyFixedAdminBar = $('body.toolbar-fixed #toolbar-bar');
+  var bodyFixedAdminHorizontalTray = $('body.toolbar-fixed.toolbar-tray-open.toolbar-horizontal #toolbar-item-administration-tray');
+  var adminToolbarHorizontalTray = $('body.toolbar-tray-open.toolbar-horizontal #toolbar-item-administration-tray');
 
   // Classes
   var mainMenuSubOpen = 'main-menu__sub-icon--open';
@@ -198,7 +259,13 @@
   function removeMainMenuBg() {
     mainMenuBg.remove();
     mainMenuBg = '';
-  }
+  };
+
+  // Add mobile nav background
+  function addMainMenuBg() {
+    mainMenuBg = $('<span class="js-main-menu-close" />');
+    mainMenuToggleOpen.append(mainMenuBg);
+  };
 
   // Show/Hide overlay
   function mainMenuBgToggle() {
@@ -208,10 +275,15 @@
     // Display main menu
     mainMenu.toggleClass(menuVisible);
 
-    //Scroll to main menu items
-    mainMenuActiveItem = $('.is-active');
+    //Scroll to main menu active item.
+    mainMenuActiveItem = $('#main-nav .main-menu__item--active-trail-last .is-active');
     if (mainMenuActiveItem.length) {
-      mainMenuActiveItem.get(0).scrollIntoView();
+      // Wait for the menu to slide in, currently set at 0.5s in _main-menu.scss.
+      setTimeout(function () {
+        mainMenuActiveItem.get(0).scrollIntoView({
+          behavior: "smooth"
+        });
+      }, 500);
     }
 
     // Remove the overlay if it exists
@@ -220,9 +292,7 @@
     }
     // Add overlay if needed
     else {
-        mainMenuBg = $('<span class="js-main-menu-close" />');
-        mainMenuBg.css({ top: bodyPaddingTop });
-        mainMenuToggleOpen.append(mainMenuBg);
+        addMainMenuBg();
       }
   };
 
@@ -230,7 +300,6 @@
   mainMenuToggle.click(function (e) {
     e.preventDefault();
     mainMenuBgToggle();
-    // expandParents();
   });
 
   // Display Sub-nav
@@ -248,16 +317,21 @@
   });
 
   // Calculate body padding top (to accommodate the admin menu)
-  function calculateBodyPaddingTop() {
-    bodyPaddingTop = parseInt(body.css('padding-top'), 10);
+  function calculateAdminToolbarHeight() {
+    adminToolbarHorizontalTray = $('body.toolbar-tray-open.toolbar-horizontal #toolbar-item-administration-tray');
+    adminToolbarHeight = (parseInt(adminToolbar.outerHeight()) || 0) + (parseInt(adminToolbarHorizontalTray.outerHeight()) || 0);
+  };
+
+  // Calculate body padding top (to accommodate the admin menu)
+  function calculateBodyFixedAdminBarHeight() {
+    bodyFixedAdminBar = $('body.toolbar-fixed #toolbar-bar');
+    bodyFixedAdminHorizontalTray = $('body.toolbar-fixed.toolbar-tray-open.toolbar-horizontal #toolbar-item-administration-tray');
+    bodyFixedAdminBarHeight = (parseInt(bodyFixedAdminBar.outerHeight()) || 0) + (parseInt(bodyFixedAdminHorizontalTray.outerHeight()) || 0);
   };
 
   // Calculate header height (to accommodate the fixed header bar)
   function calculateHeaderHeight() {
-    headerHeight = 0;
-    if (header.css('position') == 'fixed') {
-      headerHeight = parseInt(header.css('height'), 10);
-    }
+    headerHeight = parseInt(header.outerHeight()) || 0;
   };
 
   function setSideMenuSubMaxHeight() {
@@ -273,13 +347,26 @@
   // Set the height and position of the main-nav and header bar
   function calculateMenuHeights() {
     dom.ready(function () {
-      winH = parseInt(win.height(), 10);
-      calculateBodyPaddingTop();
+      winH = parseInt(win.outerHeight(), 10);
+      winW = parseInt(win.outerWidth(), 10);
+      bodyFixedAdminBar = $('body.toolbar-fixed #toolbar-bar');
+
+      // calculateBodyPaddingTop();
       calculateHeaderHeight();
-      maxHeight = winH - bodyPaddingTop;
-      mainMenu.css({ 'top': bodyPaddingTop, 'max-height': maxHeight });
-      header.css({ 'top': bodyPaddingTop });
-      layoutContainer.css({ 'margin-top': headerHeight });
+      calculateAdminToolbarHeight();
+      calculateBodyFixedAdminBarHeight();
+
+      maxHeight = winH - adminToolbarHeight;
+      mainMenu.css({ 'max-height': maxHeight });
+
+      if (winW <= 900) {
+        body.css({ 'padding-top': adminToolbarHeight + headerHeight });
+      } else {
+        body.css({ 'padding-top': adminToolbarHeight });
+      }
+
+      header.css({ 'top': adminToolbarHeight });
+
       setSideMenuSubMaxHeight();
     });
   };
@@ -296,27 +383,54 @@
         if (mainMenuBg.length) {
           removeMainMenuBg();
         }
+        // Class may remain if initial load is in mobile.
+        header.removeClass("scrollUp");
+
+        // Margin may have been added.
+        header.css({ 'margin-top': '' });
+
         // Remove noscroll class from body
         body.removeClass(noscroll);
         // Hide mobile menu
         mainMenu.removeClass(menuVisible);
       }
-      // setHeaderMarginTop();
+
       calculateMenuHeights();
     }, 100, "Main menu - window resize");
   });
 
-  calculateBodyPaddingTop();
-  var c;
-
+  var lastScrollTop = 0;
   win.scroll(function () {
-    currentScrollTop = parseInt(win.scrollTop());
-    if (c < currentScrollTop && currentScrollTop > bodyPaddingTop + headerHeight) {
-      header.addClass("scrollUp");
-    } else if (c > currentScrollTop && !(currentScrollTop <= headerHeight)) {
-      header.removeClass("scrollUp");
+    if (winW <= 900) {
+      currentScrollTop = parseInt(win.scrollTop());
+
+      // Scrolling down the page.
+      if (lastScrollTop < currentScrollTop) {
+
+        if (currentScrollTop > adminToolbarHeight + headerHeight) {
+          header.removeClass("show-menu-bar");
+        }
+      } else {
+        // Scrolling up the page.
+        // Current scroll is past top menus.
+        if (currentScrollTop > adminToolbarHeight - bodyFixedAdminBarHeight) {
+          header.css({ 'top': -headerHeight });
+          header.css({ 'margin-top': bodyFixedAdminBarHeight });
+          header.removeClass("no-transition");
+          header.addClass("scrollUp");
+          header.addClass("show-menu-bar");
+
+          // Current scroll is withing top menus.
+        } else {
+          header.css({ 'top': adminToolbarHeight });
+          header.css({ 'margin-top': '' });
+          header.addClass("no-transition");
+          header.removeClass("scrollUp");
+          header.removeClass("show-menu-bar");
+        }
+      }
+      lastScrollTop = currentScrollTop;
     }
-    c = currentScrollTop;
   });
 })(jQuery, Drupal);
 //# sourceMappingURL=scripts-styleguide.js.map
