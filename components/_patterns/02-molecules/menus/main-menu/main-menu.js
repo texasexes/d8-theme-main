789d958b59a082cf7a93e1df40e3968b7161d799
Autif Kamal <autif.kamal@texasexes.org>
2022-02-10 14:39:36 -0600

    chevron fix

--- components/_patterns/02-molecules/menus/main-menu/main-menu.js
+++ components/_patterns/02-molecules/menus/main-menu/main-menu.js
@@ -82,8 +82,8 @@
   // Close menu when page loads for "Find a Chapter or Network"
   $(document).ready(function () {
     if (document.baseURI.endsWith('find-chapter-or-network')) {
-      jQuery('.side-menu__dropdown--level-0').removeClass('js-visible');
-      jQuery('.side-menu__dropdown--level-0').removeClass('side-menu__sub-icon--open');
+      $('.side-menu__dropdown--level-0').removeClass('js-visible');
+      $('.side-menu__dropdown--level-0').removeClass('side-menu__sub-icon--open');
     }
   })
 
